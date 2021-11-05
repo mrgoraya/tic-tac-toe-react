02c928259d3c75a9fe5fa244df83f4ff7a4d0e64
mehran.rasheed <mehran.rasheed@incloud.de>
2021-11-05 13:38:19 +0100

    changes type to interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,27 +1,27 @@
-export type LayoutProps = {
-  gap: number,
+export interface LayoutProps {
+  gap: number;
 };
 
-export type BoardProps = {
-  board: BoardState,
-  onClick: (square: number) => void 
+export interface BoardProps {
+  board: BoardState;
+  onClick: (square: number) => void;
 }
 
-export type SquareProps = {
-  value: Value,
-  onClick: () => void
+export interface SquareProps {
+  value: Value;
+  onClick: () => void;
 }
 
-export type LogProps = {
-  history: BoardState[],
-  jumpTo: (step: number) => void
+export interface LogProps {
+  history: BoardState[];
+  jumpTo: (step: number) => void;
 }
 
 export type Value = "X" | "O" | null;
 
 export type BoardState = Value[];
 
-export type GameState = {
+export interface GameState {
   history: BoardState[],
   step: number
 };
